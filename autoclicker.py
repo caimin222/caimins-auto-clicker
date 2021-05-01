ec6480001b6abb5639a3bc1ab817a07e3e0c8e97
caimin222 <caiminoneill@gmail.com>
2021-05-01 15:34:58 -0700

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -6,7 +6,7 @@ import random
 import win32api, win32con
 
 
-#defines a click function
+#defines what a click is
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
