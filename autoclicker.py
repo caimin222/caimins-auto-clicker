c8caf58e32e4e920d35d5dc806024d28e305840d
caimin222 <caiminoneill@gmail.com>
2021-05-01 18:50:03 -0700

    fixed the delay
    the click delay was between not after the click which caused issues

--- autoclicker.py
+++ autoclicker.py
@@ -10,12 +10,13 @@ import win32api, win32con
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(0.08)#delay between clicks
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+    time.sleep(0.08)
 #the clicker
 #use this to change the key https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 while True:
     a = win32api.GetKeyState(0x06)#the key
     if a < 0:
+     click(960,540)#where to click
      click(960,540)#where to click
 	
