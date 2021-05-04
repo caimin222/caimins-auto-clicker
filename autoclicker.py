f01b2b4ca30bac5ea2e48ace5aa6ca30584b8396
caimin222 <caiminoneill@gmail.com>
2021-05-04 00:40:24 -0700

    more easy to use
    just simplifys some of the concepts for people who dont understand code

--- autoclicker.py
+++ autoclicker.py
@@ -5,18 +5,20 @@ import keyboard
 import random
 import win32api, win32con
 
+#use this to change the key https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+key = 0x06 #the key u hold to auto click
 
 #defines what a click is
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-    time.sleep(0.075)
+    time.sleep(0.075)#delay between clicks 
+
 #the clicker
-#use this to change the key https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
 while True:
-    a = win32api.GetKeyState(0x06)#the key
+    a = win32api.GetKeyState(key)
     if a < 0:
-     click(960,540)#where to click
+     click(960,540)#position of the click 
      
 	
