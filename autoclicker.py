81957ae1656d43b9c5da44067b6f52e4a35cb96a
caimin222 <caiminoneill@gmail.com>
2021-05-10 18:28:28 -0700

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -6,31 +6,35 @@ import keyboard
 import random
 import win32api, win32con
 
+#use this to change the key https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+key = 0x06 #the key u hold to auto click
 
 
+print("Loading please wait")
+
+
+min = 0.05
+max = 0.06
+min2 = 0.04
+max2 = 0.06
+min3 = 0.05
+max3 = 0.07 
 
 #random number generator to avoid detection
 #specific range
-min = 0.03
-max = 0.06
+     
 #generate a random floating point number
+ 
 b = min + (max-min)*random.random()
 
-c = min + (max-min)*random.random()
+c = min + (max2-min2)*random.random()
 
-d = min + (max-min)*random.random()
+d = min + (max3-min3)*random.random()
 
 print(b,c,d)
 
+ 
 
-
-     
-        
-
-
-#use this to change the key https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
-key = 0x06 #the key u hold to auto click
-print("Loading please wait")
 time.sleep(1)
 print("Caimins Autoclicker loaded enjoy")
 time.sleep(0.5)
@@ -59,7 +63,7 @@ def click3(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     time.sleep(0.00000003)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-    time.sleep(c)#delay between clicks
+    time.sleep(d)#delay between clicks
 
 #the clicker
 while True:
@@ -71,10 +75,3 @@ while True:
      time.sleep(0.00000001)
      click3(960,540)#position of the third click
      time.sleep(0.00000001)
-
-
-     
-
-
-    
-     
